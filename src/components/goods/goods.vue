6a09bf7cda1052117610e7b5af5dc322eeb1b8a3
Oct <Oct@NovdeMacBook-Pro.local>
2016-12-25 22:23:37 +0800

    hello vue for this project

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="goods">
+    <div class="menu-wrapper">
+      <ul>
+        <li v-for="item in goods" class="menu-item">
+          <span class="text border-1px">
+            <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
+            {{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  const ERR_OK = 0;
+  export default {
+    props: {
+        seller: {
+            type: Object
+        }
+    },
+    data() {
+      return {
+          goods: []
+      };
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'invoice', 'special'];
+        this.$http.get('/api/goods').then((response) => {
+          response = response.body;
+          if (response.errno === ERR_OK) {
+              this.goods = response.data;
+          }
+        });
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixim.styl"
+
+  .goods
+    display: flex
+    position: absolute
+    width: 100%
+    top: 174px
+    bottom: 46px
+    overflow: hidden
+    .menu-wrapper
+      flex: 0 0 80px;
+      width: 80px
+      background-colo: #f3f5f7
+      .menu-item
+        display: table
+        height: 54px
+        width: 56px
+        line-height: 14px
+        padding: 0 12px
+        .icon
+          display: inline-block
+          width: 12px
+          height: 12px
+          margin-right: 2px
+          vertical-align: top
+          background-size: 12px 12px
+          background-repeat: no-repeat
+          &.decrease
+            bg-image('decrease_3')
+          &.discount
+            bg-image('discount_3')
+          &.invoice
+            bg-image('invoice_3')
+          &.special
+            bg-image('special_3')
+        .text
+          font-size: 12px
+          display: table-cell
+          width: 56px
+          vertical-align: middle
+          border-1px(rgba(7,17,27,0.1))
+    .foods-wrapper
+      flex: 1
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,37 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import App from './App';
+import goods from './components/goods/goods.vue';
+import ratings from './components/ratings/ratings.vue';
+import seller from './components/seller/seller.vue';
+
+import './common/stylus/index.styl';
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+let app = Vue.extend(App);
+
+let router = new VueRouter({
+  linkActiveClass: 'active'
+});
+
+router.map({
+  '/goods': {
+    component: goods
+  },
+  '/ratings': {
+    component: ratings
+  },
+  '/seller': {
+    component: seller
+  }
+});
+
+// 启动路由
+router.start(app, '#app');
+
+// 默认显示goods页面
+router.go('/goods');
+
